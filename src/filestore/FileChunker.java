b68f57e044cf5b98670c372d660c68b7a654a384
Mikail Gundogdu <mikail511@me.com>
2016-06-07 17:28:24 -0700

    Created complete FileChunker class with no known bugs

---
+++ src/filestore/FileChunker.java
@@ -0,0 +1,62 @@
+package filestore;
+import java.util.*;
+import java.io.*;
+
+/**
+ * Created by Mikhail Gundogdu on 6/7/16.
+ *
+ * FileChunk class for placing 8KB chunks of specified file into a chunk directory.
+ */
+public class FileChunker {
+    //Source: http://stackoverflow.com/questions/19177994/java-read-file-and-split-into-multiple-files
+    public static void Chunk(String toChunk) throws Exception {
+        try {
+            RandomAccessFile raf = new RandomAccessFile(toChunk, "r");
+
+            String dir = "./chunks";
+            File directory = new File(dir);
+            if(!directory.mkdir()) {
+                System.out.println("Error creating chunk directory. Exiting.");
+                return;
+            }
+
+            long sourceSize = raf.length();
+            long bytesPerSplit = 8 * 1024;
+            long numSplits = sourceSize/bytesPerSplit;
+            long remainingBytes = sourceSize % numSplits;
+            StringTokenizer strTok = new StringTokenizer(toChunk, ".");
+            String path = strTok.nextToken();
+            System.out.println("path: " +  path);
+            String extension = strTok.nextToken();
+            StringTokenizer strTok2 = new StringTokenizer(path, "/");
+            String fileName = path.substring(path.lastIndexOf("/")+1);
+            System.out.println("filename: " +  fileName);
+
+            int destIx;
+            for(destIx=1; destIx <= numSplits; destIx++) {
+                BufferedOutputStream bw = new BufferedOutputStream(new FileOutputStream(dir + "/" + fileName +destIx + '.' + extension));
+                System.out.println("here " +  destIx);
+                readWrite(raf, bw, bytesPerSplit);
+                bw.close();
+            }
+
+            if(remainingBytes > 0) {
+                BufferedOutputStream bw = new BufferedOutputStream(new FileOutputStream(dir + "/" + fileName + destIx + '.' + extension));
+                readWrite(raf, bw, remainingBytes);
+                bw.close();
+            }
+
+            raf.close();
+        } catch(FileNotFoundException e){
+            System.out.println("File access error. Exiting.");
+        }
+        //long numSplits = 10; //from user input, extract it from args
+
+    }
+
+    private static void readWrite(RandomAccessFile raf, BufferedOutputStream bw, long numBytes) throws IOException {
+        byte[] buf = new byte[(int) numBytes];
+        int val = raf.read(buf);
+        if(val != -1) bw.write(buf);
+    }
+}
